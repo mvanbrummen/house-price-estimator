baa6f004ba57920022486950edd6ae61dcbf23cc
Michael Van Brummen <michaelvanbrummen@gmail.com>
2021-11-21 19:59:31 +1000

    Added suggestions

--- gateway.go
+++ gateway.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"log"
+	"net/http"
 	"sync"
 
 	"github.com/go-resty/resty/v2"
@@ -22,6 +24,15 @@ type AccessResponse struct {
 	AccessToken string `json:"access_token,omitempty"`
 }
 
+type SuggestResponse struct {
+	Suggestions []Suggestion `json:"suggestions,omitempty"`
+}
+
+type Suggestion struct {
+	PropertyId int    `json:"propertyId,omitempty"`
+	Suggestion string `json:"suggestion,omitempty"`
+}
+
 func NewPropertyGateway(baseUrl string, clientId string, clientSecret string, client *resty.Client) *PropertyGateway {
 	p := &PropertyGateway{
 		baseUrl:      baseUrl,
@@ -35,12 +46,32 @@ func NewPropertyGateway(baseUrl string, clientId string, clientSecret string, cl
 	return p
 }
 
-func (p *PropertyGateway) GetSuggestions(query string) {
+func (p *PropertyGateway) GetSuggestions(query string) (*SuggestResponse, error) {
+	resp, err := p.client.R().
+		SetQueryParam("q", query).
+		SetQueryParam("suggestionTypes", "address").
+		SetAuthToken(p.accessToken).
+		SetResult(&SuggestResponse{}).
+		Get(fmt.Sprintf("%s/property/au/v2/suggest.json", p.baseUrl))
 
+	if err != nil {
+		return nil, err
+	}
+
+	log.Println(resp)
+
+	if resp.StatusCode() != http.StatusOK {
+		return nil, errors.New(fmt.Sprintf("Suggest returned %d status code", resp.StatusCode()))
+	}
+
+	body := resp.Result().(*SuggestResponse)
+
+	return body, nil
 }
 
 func (p *PropertyGateway) GetAccessToken() {
-	resp, err := p.client.R().SetResult(&AccessResponse{}).
+	resp, err := p.client.R().
+		SetResult(&AccessResponse{}).
 		Get(fmt.Sprintf("%s/access/oauth/token?grant_type=client_credentials&client_id=%s&client_secret=%s", p.baseUrl, p.clientId, p.clientSecret))
 
 	if err != nil {
--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"log"
 	"net/http"
 	"os"
 
@@ -10,15 +11,36 @@ import (
 
 func main() {
 	client := resty.New()
-	_ = NewPropertyGateway("https://api.corelogic.asia", os.Getenv("CLIENT_ID"), os.Getenv("CLIENT_SECRET"), client)
+	gateway := NewPropertyGateway("https://api.corelogic.asia", mustGetEnv("CLIENT_ID"), mustGetEnv("CLIENT_SECRET"), client)
 
 	r := gin.Default()
 
 	r.LoadHTMLGlob("static/templates/*")
 
 	r.GET("/", func(c *gin.Context) {
-
 		c.HTML(http.StatusOK, "index.html", nil)
+	})
+
+	r.GET("/search", func(c *gin.Context) {
+		log.Println(c.Query("q"))
+
+		suggestions, err := gateway.GetSuggestions(c.Query("q"))
+
+		log.Println(suggestions)
+
+		if err != nil {
+			c.JSON(500, gin.H{
+				"error": err.Error(),
+			})
+			return
+		}
+
+		c.HTML(http.StatusOK, "suggestions.html", suggestions)
+	})
+
+	r.GET("/result/:propertyId", func(c *gin.Context) {
+
+		c.HTML(http.StatusOK, "result.html", nil)
 	})
 
 	r.GET("/health", func(c *gin.Context) {
@@ -28,4 +50,14 @@ func main() {
 	})
 
 	r.Run(":8080")
+}
+
+func mustGetEnv(key string) string {
+	e := os.Getenv(key)
+
+	if e == "" {
+		panic("Env var was not set: " + key)
+	}
+
+	return e
 }
